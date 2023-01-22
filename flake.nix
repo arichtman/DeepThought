ff68a67f9f3944eb390c430f668eac79f1ae15d2
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-01-22 18:23:27 +1000

    enhanced dev tooling

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "DeepThought theme";
+  nixConfig.bash-prompt = "\[nix-develop\]$ ";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+            zola
+        ];
+          shellHook = ''
+            zola --version
+          '';
+      };
+    });
+}
